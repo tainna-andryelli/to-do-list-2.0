3747c18d60a817e39e555a90345494aec373d07d
Tainna <tainnaandryelli@gmail.com>
2024-02-13 19:25:19 -0300

    feat: remove task

--- script.js
+++ script.js
@@ -1,68 +1,77 @@
 // localStorage.clear();
+const inputNewTask = document.getElementById("input-new-task");
+const buttonNewTask = document.getElementById("btn-new-task");
+const localStorageKey = "to-do-list";
 
-function newTask() {
-  const inputNewTask = document.getElementById("input-new-task");
-  const buttonNewTask = document.getElementById("btn-new-task");
-  const localStorageKey = "to-do-list";
-
-  function taskExists(value) {
-    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
-    if (values.length) {
-      for (let i = 0; i < values.length; i++) {
-        if (values[i].name === value) {
-          return true;
-        }
+function taskExists(value) {
+  let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+  if (values.length) {
+    for (let i = 0; i < values.length; i++) {
+      if (values[i].name === value) {
+        return true;
       }
-      return false;
-    } else {
-      return false;
     }
+    return false;
+  } else {
+    return false;
+  }
+}
+
+function addNewTask() {
+  if (!inputNewTask.value) {
+    // active modal
+    alert("Digite uma tarefa.");
+  } else if (taskExists(inputNewTask.value)) {
+    alert("Já existe uma task com esse nome.");
+    inputNewTask.value = "";
+  } else {
+    // increments to localStorage
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+
+    values.push({
+      name: inputNewTask.value,
+    });
+
+    localStorage.setItem(localStorageKey, JSON.stringify(values));
+
+    showValues();
+    inputNewTask.value = "";
+  }
+}
+
+buttonNewTask.addEventListener("click", addNewTask);
+
+function showValues() {
+  const alert = document.querySelector(".alert");
+  let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+  let todolist = document.getElementById("to-do-list");
+  todolist.innerHTML = "";
+
+  if (values.length) {
+    alert.classList.add("remove");
+  } else {
+    alert.classList.remove("remove");
   }
 
-  function addNewTask() {
-    if (!inputNewTask.value) {
-      // active modal
-      alert("Digite uma tarefa.");
-    } else if (taskExists(inputNewTask.value)) {
-      alert("Já existe uma task com esse nome.");
-      inputNewTask.value = "";
-    } else {
-      // increments to localStorage
-      let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+  for (let i = 0; i < values.length; i++) {
+    todolist.innerHTML += `<li>${values[i].name} <button class="btn-remove-task"></button></li>`;
+  }
+  let btnRemove = document.querySelectorAll(".btn-remove-task");
+  remove(btnRemove);
+}
+showValues();
 
-      values.push({
-        name: inputNewTask.value,
-      });
+function remove(btnRemoveOK) {
+  btnRemoveOK.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      let values = JSON.parse(localStorage.getItem(localStorageKey));
+      values.splice(index, 1);
 
       localStorage.setItem(localStorageKey, JSON.stringify(values));
-
       showValues();
-      inputNewTask.value = "";
-    }
-  }
-
-  buttonNewTask.addEventListener("click", addNewTask);
-
-  function showValues() {
-    const alert = document.querySelector(".alert");
-    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
-    let todolist = document.getElementById("to-do-list");
-    todolist.innerHTML = "";
-
-    if (values.length) {
-      alert.classList.add("remove");
-    } else {
-      alert.classList.remove("remove");
-    }
-
-    for (let i = 0; i < values.length; i++) {
-      todolist.innerHTML += `<li>${values[i].name}<button id="btn-remove-task"></button></li>`;
-    }
-  }
-
-  showValues();
+    });
+  });
 }
-newTask();
 
 function colorMode() {
   const dark = document.getElementById("dark");
